89df5c010f105ffa5ab7dd1a3087c9a1536bfba6
mhrecin <mhrecin@gmail.com>
2020-09-11 11:29:38 -0400

    update-to-project2

--- rain.js
+++ rain.js
@@ -1,1 +1,14 @@
 rain.js
+console.log("Hrecin-part-two")
+
+let rainfall = prompt("How many inches of rain fell?");
+ let rainfallInches = "*";
+ console.log("rain" + rainfallInches.repeat(rainfall));
+
+let rain = prompt("Did you use fertilizer?");
+console.log(fertilizer);
+if("yes");
+
+
+let premium = prompt("Did you use premium or regular fertilizer?");
+console.log(premium);
