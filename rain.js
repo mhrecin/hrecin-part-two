034c0fc07bbc0114ba005a88483570f9106f43ec
mhrecin <mhrecin@gmail.com>
2020-09-16 15:29:03 -0400

    update to prject 2

--- rain.js
+++ rain.js
@@ -2,13 +2,28 @@ rain.js
 console.log("Hrecin-part-two")
 
 let rainfall = prompt("How many inches of rain fell?");
- let rainfallInches = "*";
- console.log("rain" + rainfallInches.repeat(rainfall));
+ 
+let bushels = 50;
 
-let rain = prompt("Did you use fertilizer?");
-console.log(fertilizer);
-if("yes");
+let rainfallInches = "*";
+    console.log("rain" + rainfallInches.repeat(rainfall));
 
+let fertilizer = prompt("Did you use fertilizer?");
+if (fertilizer === "no") {
+    if (rainfall > 20){
+    bushels = bushels * .9;
+    console.log("The crop yields " + (bushels * .9) + " bushels per year");
+    }
+    if (rainfall < 10){
+    bushels = bushels * .8;
+    console.log("The crop yields " + (bushels * .8) + " bushels per year");
+    }
+    if (rainfall === 11){
+    bushels = bushels * 1;  
+    console.log("The crop yields " + (bushels * 1) + " bushels per year");  
+    }
+}
 
+if (fertilizer === "yes") {
 let premium = prompt("Did you use premium or regular fertilizer?");
-console.log(premium);
+}
